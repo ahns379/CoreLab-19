1ce10e8e6c08bc0208e429177e7511903fc227be
ahns379 <42782101+ahns379@users.noreply.github.com>
2019-04-01 23:37:35 -0400

    Update index.js

--- STUDIO/project2/js/index.js
+++ STUDIO/project2/js/index.js
@@ -1,8 +1,8 @@
 var active = keyNumber => {
-   var pianoNotes = document.querySelector(`.note__${keyNumber}`)
-   var key = document.querySelector(`.code__${keyNumber}`)
-   pianoNotes.play();
-   pianoNotes.currentTime = 0;
+	var pianoNotes = document.querySelector(`.note__${keyNumber}`)
+	var key = document.querySelector(`.code__${keyNumber}`)
+	pianoNotes.play();
+	pianoNotes.currentTime = 0;
 }
 window.addEventListener("keydown", e => active(e.keyCode))
 
@@ -13,14 +13,14 @@ function randomInt(input){
 	return rand;
 }
 function type(event){
-  $("h1").addClass("fade");
+	$("h1").addClass("fade");
 	var letters = document.createElement("h2");
-  var w = window.innerWidth;
-  var h = window.innerHeight;
+	var w = window.innerWidth;
+	var h = window.innerHeight;
 	letters.innerHTML = event.key;
 	letters.style.top = randomInt(h) + "px";
 	letters.style.left = randomInt(w) + "px";
-  $("h2").addClass("animate");
+	$("h2").addClass("animate");
 	document.body.appendChild(letters);
 }
-  document.onkeypress = type;
+document.onkeypress = type;
