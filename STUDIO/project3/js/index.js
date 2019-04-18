f1094bb49f599ad65a4a57a250b8711af83558bd
ahns379 <ahns379@newschool.edu>
2019-04-18 15:33:27 -0400

    proj3

--- STUDIO/project3/js/index.js
+++ STUDIO/project3/js/index.js
@@ -1,5 +1,5 @@
 $("body").css("transform", "perspective(10000px) rotateY(50deg)");
-$("html").css("cssText", "margin-top: 5%; margin-left: -8%; width: 60%;");
+$("html").css("cssText", "margin-top: 5%; margin-left: 0; width: 60%;");
 
 var style = document.createElement("style");
 document.body.appendChild(style).innerHTML = "
@@ -18,24 +18,33 @@ $("html").append("<div class='tag-name'></div>");
 $("*").mouseover(function(event) {
   event.stopPropagation();
   var elementType = this.tagName;
-  var ALL = $(this).parents().map(function () {
-      return this.tagName;
+  var ALL = $(this).parents()
+    .map(function () {
+      return "<" + this.tagName + ">";
     })
     .get()
-    .join(" ");
+    .reverse()
+    .join("  ");
 
-  $(".tag-name").text("<" + elementType + "> \n" );
+  var text = ALL + " <" + elementType + ">";
 
-  $(".tag-name").append(ALL);
+  $(".tag-name").text(text);
 
   $(".tag-name").css({
     "font-family": "monospace",
-    "left": "160%" ,
+    "left": "0",
+    "top": "0",
+    "width": "100vw",
     "font-size": "1.5em",
-    "word-spacing": "100vw",
+    "margin-top": "-9%",
+    "padding": "20px",
     "background-color": "rgb(41,44,52)",
-		"color": "rgb(210,115,119)"
+    "color": "rgb(210,115,119)"
   });
   $(".tag-name").css("position","absolute");
   $(".tag-name").css("top",$(document).scrollTop() + "px");
+
+
 });
+
+document.scrollBy(0, 1);
